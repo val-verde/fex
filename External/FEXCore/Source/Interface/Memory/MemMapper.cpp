f9025a6622ceb995d16d8f158fe66a0f1d566a17
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2020-10-08 10:33:49 +0300

    Hangover patches

--- External/FEXCore/Source/Interface/Memory/MemMapper.cpp
+++ External/FEXCore/Source/Interface/Memory/MemMapper.cpp
@@ -7,7 +7,7 @@
 namespace FEXCore::Memory {
 
   void *MemMapper::MapRegion(uint64_t Offset, size_t Size, bool Fixed, bool RelativeToBase) {
-    return MapRegion(Offset, Size, PROT_READ | PROT_WRITE, Fixed, RelativeToBase);
+    return (void *)Offset;
   }
 
   void *MemMapper::ChangeMappedRegion(uint64_t Offset, size_t Size, uint32_t Flags, bool Fixed, bool RelativeToBase) {
@@ -29,41 +29,14 @@ namespace FEXCore::Memory {
   }
 
   void *MemMapper::MapRegion(uint64_t Offset, size_t Size, uint32_t Flags, bool Fixed, bool RelativeToBase) {
-    uintptr_t PtrOffset = Offset;
-
-    if (RelativeToBase) {
-      PtrOffset = reinterpret_cast<uintptr_t>(SHM->Object.Ptr) + Offset;
-    }
-
-    void *Ptr = mmap(reinterpret_cast<void*>(PtrOffset), Size, Flags,
-      MAP_PRIVATE | (Fixed ? MAP_FIXED : 0), SHM->SHMFD, Offset);
-
-    if (Ptr == MAP_FAILED) {
-      LogMan::Msg::A("Failed to map memory region [0x%lx, 0x%lx)", Offset, Offset + Size);
-      return nullptr;
-    }
-
-    MappedRegions.emplace_back(MemRegion{Ptr, Offset, Size});
-
-    return Ptr;
+    return (void *)Offset;
   }
 
   void MemMapper::UnmapRegion(void *Ptr, size_t Size) {
-    auto it = std::find(MappedRegions.begin(), MappedRegions.end(), Ptr);
-    if (it != MappedRegions.end()) {
-      munmap(Ptr, Size);
-      MappedRegions.erase(it);
-    }
   }
 
   void *MemMapper::GetPointer(uint64_t Offset) {
-    for (auto const &Region : MappedRegions) {
-      if (Region.contains(Offset)) {
-        return reinterpret_cast<void*>(reinterpret_cast<uintptr_t>(Region.Ptr) + (Offset - Region.Offset));
-      }
-    }
-
-    return nullptr;
+    return (void *)Offset;
   }
 
 #ifndef NDEBUG

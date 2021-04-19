c4640c2411e999816aba0108efddba84c19b5de9
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2021-04-19 18:57:07 +0300

    IR: Remove Entry from OP_HEADER, pass as parameter to CompileCode

--- External/FEXCore/Source/Interface/Core/Core.cpp
+++ External/FEXCore/Source/Interface/Core/Core.cpp
@@ -170,7 +170,7 @@ namespace DefaultFallbackCore {
     void Initialize() override {}
     bool NeedsOpDispatch() override { return false; }
 
-    void *CompileCode(FEXCore::IR::IRListView const *IR, FEXCore::Core::DebugData *DebugData, FEXCore::IR::RegisterAllocationData *RAData) override {
+    void *CompileCode(uint64_t Entry, FEXCore::IR::IRListView const *IR, FEXCore::Core::DebugData *DebugData, FEXCore::IR::RegisterAllocationData *RAData) override {
       LogMan::Msg::E("Fell back to default code handler at RIP: 0x%lx", ThreadState->CurrentFrame->State.rip);
       return nullptr;
     }
@@ -907,7 +907,6 @@ namespace FEXCore::Context {
               IRList = AOTEntry->GetIRData();
               //LogMan::Msg::D("using %s + %lx -> %lx\n", file->second.fileid.c_str(), AOTEntry->first, GuestRIP);
               // relocate
-              IRList->GetHeader()->Entry = GuestRIP;
 
               RAData = AOTEntry->GetRAData();;
               DebugData = new FEXCore::Core::DebugData();
@@ -948,7 +947,7 @@ namespace FEXCore::Context {
     }
 
     // Attempt to get the CPU backend to compile this code
-    return { Thread->CPUBackend->CompileCode(IRList, DebugData, RAData), IRList, DebugData, RAData, GeneratedIR, StartAddr, Length};
+    return { Thread->CPUBackend->CompileCode(GuestRIP, IRList, DebugData, RAData), IRList, DebugData, RAData, GeneratedIR, StartAddr, Length};
   }
 
   static bool readAll(int fd, void *data, size_t size) {
@@ -980,7 +979,7 @@ namespace FEXCore::Context {
     struct stat fileinfo;
     if (fstat(streamfd, &fileinfo) < 0)
       return false;
-    void *fileptr = mmap(nullptr, (fileinfo.st_size + 4095) & ~4095, PROT_READ | PROT_WRITE, MAP_PRIVATE, streamfd, 0);
+    void *fileptr = mmap(nullptr, (fileinfo.st_size + 4095) & ~4095, PROT_READ, MAP_PRIVATE, streamfd, 0);
 
     if (fileptr == MAP_FAILED)
       return false;
--- External/FEXCore/Source/Interface/Core/JIT/Arm64/JIT.cpp
+++ External/FEXCore/Source/Interface/Core/JIT/Arm64/JIT.cpp
@@ -668,7 +668,7 @@ bool Arm64JITCore::IsInlineEntrypointOffset(const IR::OrderedNodeWrapper& WNode,
   if (OpHeader->Op == IR::IROps::OP_INLINEENTRYPOINTOFFSET) {
     auto Op = OpHeader->C<IR::IROp_InlineEntrypointOffset>();
     if (Value) {
-      *Value = IR->GetHeader()->Entry + Op->Offset;
+      *Value = Entry + Op->Offset;
     }
     return true;
   } else {
@@ -693,17 +693,18 @@ bool Arm64JITCore::IsGPR(uint32_t Node) {
   return Class == IR::GPRClass || Class == IR::GPRFixedClass;
 }
 
-void *Arm64JITCore::CompileCode([[maybe_unused]] FEXCore::IR::IRListView const *IR, [[maybe_unused]] FEXCore::Core::DebugData *DebugData, FEXCore::IR::RegisterAllocationData *RAData) {
+void *Arm64JITCore::CompileCode(uint64_t Entry, [[maybe_unused]] FEXCore::IR::IRListView const *IR, [[maybe_unused]] FEXCore::Core::DebugData *DebugData, FEXCore::IR::RegisterAllocationData *RAData) {
   using namespace aarch64;
   JumpTargets.clear();
   uint32_t SSACount = IR->GetSSACount();
 
+  this->Entry = Entry;
   this->RAData = RAData;
 
   auto HeaderOp = IR->GetHeader();
 
   #ifndef NDEBUG
-  LoadConstant(x0, HeaderOp->Entry);
+  LoadConstant(x0, Entry);
   #endif
 
   this->IR = IR;
@@ -735,7 +736,7 @@ void *Arm64JITCore::CompileCode([[maybe_unused]] FEXCore::IR::IRListView const *
   // X4-r18 = RA
 
   auto Buffer = GetBuffer();
-  auto Entry = Buffer->GetOffsetAddress<uint64_t>(GetCursorOffset());
+  auto GuestEntry = Buffer->GetOffsetAddress<uint64_t>(GetCursorOffset());
 
  if (CTX->GetGdbServerStatus()) {
     aarch64::Label RunBlock;
@@ -752,7 +753,7 @@ void *Arm64JITCore::CompileCode([[maybe_unused]] FEXCore::IR::IRListView const *
     cbz(w0, &RunBlock);
     {
       // Make sure RIP is syncronized to the context
-      LoadConstant(x0, HeaderOp->Entry);
+      LoadConstant(x0, Entry);
       str(x0, MemOperand(STATE, offsetof(FEXCore::Core::CpuStateFrame, State.rip)));
 
       // Stop the thread
@@ -826,15 +827,15 @@ void *Arm64JITCore::CompileCode([[maybe_unused]] FEXCore::IR::IRListView const *
   FinalizeCode();
 
   auto CodeEnd = Buffer->GetOffsetAddress<uint64_t>(GetCursorOffset());
-  CPU.EnsureIAndDCacheCoherency(reinterpret_cast<void*>(Entry), CodeEnd - reinterpret_cast<uint64_t>(Entry));
+  CPU.EnsureIAndDCacheCoherency(reinterpret_cast<void*>(GuestEntry), CodeEnd - reinterpret_cast<uint64_t>(GuestEntry));
 
   if (DebugData) {
-    DebugData->HostCodeSize = reinterpret_cast<uintptr_t>(CodeEnd) - reinterpret_cast<uintptr_t>(Entry);
+    DebugData->HostCodeSize = reinterpret_cast<uintptr_t>(CodeEnd) - reinterpret_cast<uintptr_t>(GuestEntry);
   }
 
   this->IR = nullptr;
 
-  return reinterpret_cast<void*>(Entry);
+  return reinterpret_cast<void*>(GuestEntry);
 }
 
 uint64_t Arm64JITCore::ExitFunctionLink(Arm64JITCore *core, FEXCore::Core::CpuStateFrame *Frame, uint64_t *record) {

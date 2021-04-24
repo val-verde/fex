fcbc0cff909142509c1683b31bc1fc79d5d387af
Stefanos Kornilios Mitsis Poiitidis <stef@skmp.dev>
2021-04-24 04:27:54 +0300

    AOTIR: Parallel update script

--- Scripts/FEXUpdateAOTIRCache.sh
+++ Scripts/FEXUpdateAOTIRCache.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 FEX={$1:FEXLoader}
+FEX=`which "$FEX"`
+FEX=`realpath Bin/FEXLoader`
+FILES=""
 for fileid in ~/.fex-emu/aotir/*.path; do
 	filename=`cat "$fileid"`
 	if [ -f "${fileid%.path}.aotir" ]; then
 		echo "$filename has already been generated"
 	else
-		echo "Processing $filename ($fileid)"
-		Bin/FEXLoader --aotirgenerate  "$filename"
+		echo "Will process $filename ($fileid)"
+		FILES="$filename
+$FILES"
 	fi
 done
+
+parallel $FEX --aotirgenerate {} <<< $FILES
